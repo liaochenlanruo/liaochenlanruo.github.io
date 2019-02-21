d47f195732c9a00e7b430d3f7cc65eb408cd3184
liaochenlanruo <liaochenlanruo@webmail.hzau.edu.cn>
2019-02-21 21:15:45 +0800

    Site updated: 2019-02-21 21:15:42

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://liaochenlanruo.github.io/imgs/min_photos/' + data.link[i] + '.min.jpg';
-          var src = 'https://liaochenlanruo.github.io/imgs/photos/' + data.link[i];
+          var minSrc = 'https://liaochenlanruo.github.io/liaochenlanruo/liaochenlanruo/master/photos/' + data.link[i] + '.min.jpg';
+          var src = 'https://liaochenlanruo.github.io/liaochenlanruo/liaochenlanruo/master/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '.jpg';
